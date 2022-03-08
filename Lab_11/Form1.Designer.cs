6be0ec30bfd6c0af976a057fe02b22087a81f788
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2022-03-08 19:22:19 +0600

    Lab_11
    (SWE 4202)

---
+++ Lab_11/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Lab_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.box1 = new System.Windows.Forms.Button();
+            this.box6 = new System.Windows.Forms.Button();
+            this.box5 = new System.Windows.Forms.Button();
+            this.box4 = new System.Windows.Forms.Button();
+            this.box3 = new System.Windows.Forms.Button();
+            this.box2 = new System.Windows.Forms.Button();
+            this.box9 = new System.Windows.Forms.Button();
+            this.box8 = new System.Windows.Forms.Button();
+            this.box7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.play_again = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.black_box = new System.Windows.Forms.TextBox();
+            this.red_box = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.draw_box = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // box1
+            // 
+            this.box1.Location = new System.Drawing.Point(714, 84);
+            this.box1.Name = "box1";
+            this.box1.Size = new System.Drawing.Size(80, 69);
+            this.box1.TabIndex = 24;
+            this.box1.UseVisualStyleBackColor = true;
+            this.box1.Click += new System.EventHandler(this.box1_Click);
+            // 
+            // box6
+            // 
+            this.box6.Location = new System.Drawing.Point(886, 159);
+            this.box6.Name = "box6";
+            this.box6.Size = new System.Drawing.Size(80, 69);
+            this.box6.TabIndex = 25;
+            this.box6.UseVisualStyleBackColor = true;
+            this.box6.Click += new System.EventHandler(this.box6_Click);
+            // 
+            // box5
+            // 
+            this.box5.Location = new System.Drawing.Point(800, 159);
+            this.box5.Name = "box5";
+            this.box5.Size = new System.Drawing.Size(80, 69);
+            this.box5.TabIndex = 26;
+            this.box5.UseVisualStyleBackColor = true;
+            this.box5.Click += new System.EventHandler(this.box5_Click);
+            // 
+            // box4
+            // 
+            this.box4.Location = new System.Drawing.Point(714, 159);
+            this.box4.Name = "box4";
+            this.box4.Size = new System.Drawing.Size(80, 69);
+            this.box4.TabIndex = 27;
+            this.box4.UseVisualStyleBackColor = true;
+            this.box4.Click += new System.EventHandler(this.box4_Click);
+            // 
+            // box3
+            // 
+            this.box3.Location = new System.Drawing.Point(886, 84);
+            this.box3.Name = "box3";
+            this.box3.Size = new System.Drawing.Size(80, 69);
+            this.box3.TabIndex = 28;
+            this.box3.UseVisualStyleBackColor = true;
+            this.box3.Click += new System.EventHandler(this.box3_Click);
+            // 
+            // box2
+            // 
+            this.box2.Location = new System.Drawing.Point(800, 84);
+            this.box2.Name = "box2";
+            this.box2.Size = new System.Drawing.Size(80, 69);
+            this.box2.TabIndex = 29;
+            this.box2.UseVisualStyleBackColor = true;
+            this.box2.Click += new System.EventHandler(this.box2_Click);
+            // 
+            // box9
+            // 
+            this.box9.Location = new System.Drawing.Point(886, 234);
+            this.box9.Name = "box9";
+            this.box9.Size = new System.Drawing.Size(80, 69);
+            this.box9.TabIndex = 30;
+            this.box9.UseVisualStyleBackColor = true;
+            this.box9.Click += new System.EventHandler(this.box9_Click);
+            // 
+            // box8
+            // 
+            this.box8.Location = new System.Drawing.Point(800, 234);
+            this.box8.Name = "box8";
+            this.box8.Size = new System.Drawing.Size(80, 69);
+            this.box8.TabIndex = 31;
+            this.box8.UseVisualStyleBackColor = true;
+            this.box8.Click += new System.EventHandler(this.box8_Click);
+            // 
+            // box7
+            // 
+            this.box7.Location = new System.Drawing.Point(714, 234);
+            this.box7.Name = "box7";
+            this.box7.Size = new System.Drawing.Size(80, 69);
+            this.box7.TabIndex = 32;
+            this.box7.UseVisualStyleBackColor = true;
+            this.box7.Click += new System.EventHandler(this.box7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(755, 335);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 32);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "Tic-Tac-Toe";
+            // 
+            // play_again
+            // 
+            this.play_again.Location = new System.Drawing.Point(777, 384);
+            this.play_again.Name = "play_again";
+            this.play_again.Size = new System.Drawing.Size(121, 44);
+            this.play_again.TabIndex = 34;
+            this.play_again.Text = "Play Again";
+            this.play_again.UseVisualStyleBackColor = true;
+            this.play_again.Click += new System.EventHandler(this.play_again_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(181, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 32);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "Red     :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(181, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 32);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "Black  :";
+            // 
+            // black_box
+            // 
+            this.black_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.black_box.Location = new System.Drawing.Point(319, 121);
+            this.black_box.Name = "black_box";
+            this.black_box.Size = new System.Drawing.Size(100, 38);
+            this.black_box.TabIndex = 37;
+            // 
+            // red_box
+            // 
+            this.red_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.red_box.Location = new System.Drawing.Point(319, 196);
+            this.red_box.Name = "red_box";
+            this.red_box.Size = new System.Drawing.Size(100, 38);
+            this.red_box.TabIndex = 38;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(181, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 32);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "Draw    :";
+            // 
+            // draw_box
+            // 
+            this.draw_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.draw_box.Location = new System.Drawing.Point(319, 271);
+            this.draw_box.Name = "draw_box";
+            this.draw_box.Size = new System.Drawing.Size(100, 38);
+            this.draw_box.TabIndex = 40;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1168, 450);
+            this.Controls.Add(this.draw_box);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.red_box);
+            this.Controls.Add(this.black_box);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.play_again);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.box7);
+            this.Controls.Add(this.box8);
+            this.Controls.Add(this.box9);
+            this.Controls.Add(this.box2);
+            this.Controls.Add(this.box3);
+            this.Controls.Add(this.box4);
+            this.Controls.Add(this.box5);
+            this.Controls.Add(this.box6);
+            this.Controls.Add(this.box1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button box1;
+        private System.Windows.Forms.Button box6;
+        private System.Windows.Forms.Button box5;
+        private System.Windows.Forms.Button box4;
+        private System.Windows.Forms.Button box3;
+        private System.Windows.Forms.Button box2;
+        private System.Windows.Forms.Button box9;
+        private System.Windows.Forms.Button box8;
+        private System.Windows.Forms.Button box7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button play_again;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox black_box;
+        private System.Windows.Forms.TextBox red_box;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox draw_box;
+    }
+}
+
---
+++ Lab_11/Form1.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_11
+{
+    public partial class Form1 : Form
+    {
+
+        public int i = 0;
+
+        public int flag = 0;
+
+        int red_win = 0, black_win = 0, draw = 0;
+
+
+        public void check_win()
+        {
+            if(box1.BackColor == Color.Black && box2.BackColor == Color.Black && box3.BackColor == Color.Black)
+            {
+                MessageBox.Show("BLACK WON");
+                flag = 1;
+                black_win++;
+
+                black_box.Text = Convert.ToString(black_win);
+            }
+
+            else if(box4.BackColor == Color.Black && box5.BackColor == Color.Black && box6.BackColor == Color.Black)
+            {
+                MessageBox.Show("BLACK WON");
+                flag = 1;
+                black_win++;
+
+                black_box.Text = Convert.ToString(black_win);
+            }
+
+            else if(box7.BackColor == Color.Black && box8.BackColor == Color.Black && box9.BackColor == Color.Black)
+            {
+                MessageBox.Show("BLACK WON");
+                flag = 1;
+                black_win++;
+
+                black_box.Text = Convert.ToString(black_win);
+            }
+
+            else if (box1.BackColor == Color.Black && box4.BackColor == Color.Black && box7.BackColor == Color.Black)
+            {
+                MessageBox.Show("BLACK WON");
+                flag = 1;
+                black_win++;
+
+                black_box.Text = Convert.ToString(black_win);
+            }
+
+            else if (box2.BackColor == Color.Black && box5.BackColor == Color.Black && box8.BackColor == Color.Black)
+            {
+                MessageBox.Show("BLACK WON");
+                flag = 1;
+                black_win++;
+
+                black_box.Text = Convert.ToString(black_win);
+            }
+
+            else if (box3.BackColor == Color.Black && box6.BackColor == Color.Black && box9.BackColor == Color.Black)
+            {
+                MessageBox.Show("BLACK WON");
+                flag = 1;
+                black_win++;
+
+                black_box.Text = Convert.ToString(black_win);
+            }
+
+            else if (box1.BackColor == Color.Black && box5.BackColor == Color.Black && box9.BackColor == Color.Black)
+            {
+                MessageBox.Show("BLACK WON");
+                flag = 1;
+                black_win++;
+
+                black_box.Text = Convert.ToString(black_win);
+            }
+
+            else if (box7.BackColor == Color.Black && box5.BackColor == Color.Black && box3.BackColor == Color.Black)
+            {
+                MessageBox.Show("BLACK WON");
+                flag = 1;
+                black_win++;
+
+                black_box.Text = Convert.ToString(black_win);
+            }
+
+            else if (box1.BackColor == Color.Red && box2.BackColor == Color.Red && box3.BackColor == Color.Red)
+            {
+                MessageBox.Show("RED WON");
+                flag = 1;
+                red_win++;
+
+                red_box.Text = Convert.ToString(red_win);
+            }
+
+            else if (box4.BackColor == Color.Red && box5.BackColor == Color.Red && box6.BackColor == Color.Red)
+            {
+                MessageBox.Show("RED WON");
+                flag = 1;
+                red_win++;
+
+                red_box.Text = Convert.ToString(red_win);
+            }
+
+            else if (box7.BackColor == Color.Red && box8.BackColor == Color.Red && box9.BackColor == Color.Red)
+            {
+                MessageBox.Show("RED WON");
+                flag = 1;
+                red_win++;
+
+                red_box.Text = Convert.ToString(red_win);
+            }
+
+            else if (box1.BackColor == Color.Red && box4.BackColor == Color.Red && box7.BackColor == Color.Red)
+            {
+                MessageBox.Show("RED WON");
+                flag = 1;
+                red_win++;
+
+                red_box.Text = Convert.ToString(red_win);
+            }
+
+            else if (box2.BackColor == Color.Red && box5.BackColor == Color.Red && box8.BackColor == Color.Red)
+            {
+                MessageBox.Show("RED WON");
+                flag = 1;
+                red_win++;
+
+                red_box.Text = Convert.ToString(red_win);
+            }
+
+            else if (box3.BackColor == Color.Red && box6.BackColor == Color.Red && box9.BackColor == Color.Red)
+            {
+                MessageBox.Show("RED WON");
+                flag = 1;
+                red_win++;
+
+                red_box.Text = Convert.ToString(red_win);
+            }
+
+            else if (box1.BackColor == Color.Red && box5.BackColor == Color.Red && box9.BackColor == Color.Red)
+            {
+                MessageBox.Show("RED WON");
+                flag = 1;
+                red_win++;
+
+                red_box.Text = Convert.ToString(red_win);
+            }
+
+            else if (box7.BackColor == Color.Red && box5.BackColor == Color.Red && box3.BackColor == Color.Red)
+            {
+                MessageBox.Show("RED WON");
+                flag = 1;
+                red_win++;
+
+                red_box.Text = Convert.ToString(red_win);
+            }
+
+            else if( i%9==0 && flag == 0)
+            {
+                MessageBox.Show("It's a DRAW!");
+                draw++;
+
+                draw_box.Text = Convert.ToString(draw);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            black_box.Text = Convert.ToString(0);
+
+            red_box.Text = Convert.ToString(0);
+
+            draw_box.Text = Convert.ToString(0);
+        }
+
+        private void box1_Click(object sender, EventArgs e)
+        {
+            i++;
+
+            if(i%2 == 1)
+            {
+                box1.BackColor = Color.Black;
+            }
+            
+            else
+            {
+                box1.BackColor = Color.Red;
+            }
+
+            check_win();
+        }
+
+        private void box2_Click(object sender, EventArgs e)
+        {
+            i++;
+
+            if (i % 2 == 1)
+            {
+                box2.BackColor = Color.Black;
+            }
+
+            else
+            {
+                box2.BackColor = Color.Red;
+            }
+
+            check_win();
+        }
+
+        private void box3_Click(object sender, EventArgs e)
+        {
+            i++;
+
+            if (i % 2 == 1)
+            {
+                box3.BackColor = Color.Black;
+            }
+
+            else
+            {
+                box3.BackColor = Color.Red;
+            }
+
+            check_win();
+        }
+
+        private void box4_Click(object sender, EventArgs e)
+        {
+            i++;
+
+            if (i % 2 == 1)
+            {
+                box4.BackColor = Color.Black;
+            }
+
+            else
+            {
+                box4.BackColor = Color.Red;
+            }
+
+            check_win();
+        }
+
+        private void box5_Click(object sender, EventArgs e)
+        {
+            i++;
+
+            if (i % 2 == 1)
+            {
+                box5.BackColor = Color.Black;
+            }
+
+            else
+            {
+                box5.BackColor = Color.Red;
+            }
+
+            check_win();
+        }
+
+        private void box6_Click(object sender, EventArgs e)
+        {
+            i++;
+
+            if (i % 2 == 1)
+            {
+                box6.BackColor = Color.Black;
+            }
+
+            else
+            {
+                box6.BackColor = Color.Red;
+            }
+
+            check_win();
+        }
+
+        private void box7_Click(object sender, EventArgs e)
+        {
+            i++;
+
+            if (i % 2 == 1)
+            {
+                box7.BackColor = Color.Black;
+            }
+
+            else
+            {
+                box7.BackColor = Color.Red;
+            }
+
+            check_win();
+        }
+
+        private void box8_Click(object sender, EventArgs e)
+        {
+            i++;
+
+            if (i % 2 == 1)
+            {
+                box8.BackColor = Color.Black;
+            }
+
+            else
+            {
+                box8.BackColor = Color.Red;
+            }
+
+            check_win();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void box9_Click(object sender, EventArgs e)
+        {
+            i++;
+
+            if (i % 2 == 1)
+            {
+                box9.BackColor = Color.Black;
+            }
+
+            else
+            {
+                box9.BackColor = Color.Red;
+            }
+
+            check_win();
+        }
+
+        private void play_again_Click(object sender, EventArgs e)
+        {
+            box1.BackColor = Color.White;
+            box2.BackColor = Color.White;
+            box3.BackColor = Color.White;
+            box4.BackColor = Color.White;
+            box5.BackColor = Color.White;
+            box6.BackColor = Color.White;
+            box7.BackColor = Color.White;
+            box8.BackColor = Color.White;
+            box9.BackColor = Color.White;
+
+            i = 0;
+        }
+    }
+}
